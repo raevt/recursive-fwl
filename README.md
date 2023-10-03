58f0a199b1a65b9331e5a9f580100ddbaf467178
Rae Adimer <137109904+raevt@users.noreply.github.com>
2023-10-02 22:49:19 -0400

    add

---
+++ README.md
@@ -0,0 +1,35 @@
+This repository includes a Python program to estimate multiple regression models via OLS without any matrix algebra. This is done using the Frisch-Waugh-Lovell Theorem in a cascading manner, allowing the reduction of a multiple regression into a series of simple linear regressions.
+
+# Files and usage
+
+# [multiple_regression.py](/multiple_regression.py)
+
+# What does this do? (i.e., the theory)
+
+The [Frisch-Waugh-Lovell Theorem](https://en.wikipedia.org/wiki/Frisch–Waugh–Lovell_theorem), referred to as "partialling out", allows the estimation of an independent variable's coefficient in a multiple regression through a regression of the dependent variable on the residuals from a regression of that independent variable on all other independent variables.
+
+When I first learned of this approach, I had a very rabbit-hole inducing thought: is it possible to recursively partial out regressions such that a multiple regression of any number of independent variables can be estimated with a series of simple linear regressions?
+
+Consider a regression model with one dependent variable and two independent variables. To estimate the coefficient on the first independent variable, we can run a simple linear regression of that variable on the other independent variable, and a second simple linear regression of the dependent variable on the residuals of that model. The coefficient on those residuals is the ceofficient of that independent variable in the multiple regression model.
+
+![Process for 2 independent variables](/images/two%20independent%20variables.png)
+
+The primary takeaway here is that a multiple regression with two independent variables can be calculated as two linear regressions, which can easily be estimated without matrix algebra.
+
+For a multiple regression with k independent variables:
+
+![Process for k independent variables](/images/k%20independent%20variables.png)
+
+Each variable's coefficient is estimated through a regression of y on the residuals from a regression of that variable on all other variables. For simplicity, I'll call these regressions (of each x on all other x's) "second-level regressions", because they are one "step down" from the initial regression.
+
+Each second-level regression has one fewer independent variable than the initial regression. In other words, a multiple regression of k independent variables can be estimated through regressions of y on the residuals from the k second-level regressions. Each of those second-level regressions has k-1 independent variables.
+
+This can be done recursively. Because any multiple regression can be estimated with k regressions of k-1 independent variables, and we know that a regression where k=2 can be estimated with two simple linear regressions, we can apply partialing out to a multiple regression recursively until every component regression has k=2. In other words, a multiple regression of any size can be estimated with some number of simple linear regressions. So, what is the function for that number?
+
+With k=2, the answer is 2 simple linear regressions, as shown above. With k=3, each independent variable's coefficient is regressed on the other two independent variables, creating three multiple regressions with two independent variables each. Each of those three regressions are then estimated with two linear regressions, resulting in 6 simple linear regressions.
+
+It seems the function for the number of simple linear regressions needed to estimate a multiple regression is k!:
+
+Consider a situation with 5 independent variables. We start with 5 linear regressions of 4 independent variables each. Then, 5\*4=20 linear regressions, of 3 independent variables each. Then, 20\*3=60 linear regressions, of 2 independent variables each. Then, 60\*2=120 simple linear regressions. In other words, with k=5, you need 5\*4\*3\*2\*1=120, or 5!, simple linear regressions. This pattern is the same for any value of k.
+
+In arriving at k!, I only considered what I'll call "moving down" regressions, i.e., the number of simple linear regressions resulting from partialing out regressions with k independent variables into k regressions of k-1 independent variables in a cascading manner, until only simple linear regressions remain. I did not consider the regressions used in "moving up" this chain, i.e., the regressions of the dependent variable of each model on the residuals of its partialed-out regressions.
---
+++ multiple_regression.py
@@ -0,0 +1,180 @@
+"""
+    Rae Adimer
+
+    Estimates a multiple regression using a series of partialed-out linear regressions. Returns estimated model and R-squared.
+
+    This is far from efficient, but it works. I've tested it out to 5 independent variables, running the same regressions in Stata and receiving the same results.
+
+    Be sure that the data is in a .csv, and:
+     - the first row are the names of the variables
+     - the first column is the dependent variable
+     - there are no missing observations
+"""
+
+
+def input_data(filename):
+    # Gets data from specified file, returns dictionary and list of variables
+    with open(filename, 'r') as file:
+        line_counter = 0
+        for line in file:
+            if line_counter == 0:
+                vars = line.strip().split(',')
+                data = {}
+                for n in vars:
+                    data[n] = []
+            else:
+                nums = line.strip().split(',')
+                for i in range(len(nums)):
+                    data[vars[i]].append(float(nums[i]))
+            line_counter += 1
+    
+    # convert to tuples
+    data2 = {}
+    for i in data:
+        data2[i] = tuple(data[i])
+
+    return data2, vars
+
+
+def simple_linear_regression(y, x):
+    # Estimates a simple linear regression, returns the estimated coefficient, intercept, and residuals
+
+    # generate y and x means
+    y_sum = 0
+    for i in y:
+        y_sum += i
+    y_mean = y_sum / len(y)
+    x_sum = 0
+    for i in x:
+        x_sum += i
+    x_mean = x_sum / len(x)
+
+    # coefficient
+    numerator = 0
+    denominator = 0
+    for i in range(len(y)):
+        numerator += (y[i] - y_mean) * (x[i] - x_mean)
+    for i in x:
+        denominator += (i - x_mean) ** 2
+    coefficient = numerator / denominator
+
+    # intercept
+    intercept = y_mean - (coefficient * x_mean)
+
+    # estimate model, make list of residuals
+    y_hats = []
+    for i in range(len(y)):
+        y_hats.append((coefficient * x[i]) + intercept)
+    residuals = []
+    for idx in range(len(y)):
+        residuals.append(y[idx] - y_hats[idx])
+
+    return coefficient, intercept, residuals
+
+def generate_new_vars(variables):
+    # Generates k new variable lists of k-1 variables, i.e., the regressions to run in partialing out a multiple regression.
+    new_vars_list = []
+    for i in range(1, len(variables)):
+        new_vars = [variables[i]] + (variables[1:i] + variables[i+1:])
+        new_vars_list.append(new_vars)
+    return new_vars_list
+
+def multiple_regression(data_dict, variables):
+    # Recursively estimates multiple regressions through partialing out
+    coefficients = []
+    means_list = means(data_dict)
+    y_key = variables[0]
+
+    if len(variables) == 2:
+        coeff, interc, residu = simple_linear_regression(data_dict[variables[0]], data_dict[variables[1]])
+        coefficients.append(coeff)
+        return coefficients, interc, residu
+    
+    else:
+        new_vars_list = generate_new_vars(variables)
+        coefficients = []
+
+        # This is the fun recursive bit
+        for n in new_vars_list:
+           n_coefficients, n_intercept, n_residuals = multiple_regression(data_dict.copy(), n.copy())
+           s_coefficient, s_intercept, s_residuals = simple_linear_regression(data_dict[y_key], n_residuals)
+           coefficients.append(s_coefficient)
+        
+        # Intercept calculation
+        intercept = means_list[0]
+        for n in range(len(coefficients)):
+            intercept -= (coefficients[n] * means_list[n + 1])
+        
+        # generate model
+        y_hats = []
+        for i in range(len(data_dict[y_key])):
+            y_to_add = intercept
+            for n in range(len(coefficients)):
+                y_to_add += coefficients[n] * data_dict[variables[n + 1]][i]
+            y_hats.append(y_to_add)
+        
+        # get list of residuals
+        residuals = []
+        for i in range(len(y_hats)):
+            residuals.append(data_dict[variables[0]][i] - y_hats[i])
+        # return statement
+        return coefficients, intercept, residuals
+
+def means(data_dict):
+    # Gets means of all items in data_dict, returns them
+    means = []
+    for n in data_dict:
+        sum = 0
+        for i in data_dict[n]:
+            sum += i
+        means.append(sum / len(data_dict[n]))
+    return means
+
+def output(data_dict, variables, coefficients, intercept, residuals):
+    # Formats and prints output, including calculating r-squared
+
+    # Variables information
+    print("Variables: ", end="")
+    for i in variables:
+        if i == variables[-1]:
+            print(i)
+        else:
+            print(i, end=", ")
+    print(f"Dependent variables: 1 ({variables[0]})\nIndependent variables: {len(variables)-1}\nObservations: {len(data_dict[variables[0]])}")
+
+    # Estimated model
+    print(f"{variables[0]} = ", end="")
+    for n in range(len(variables)-1):
+        print(f"{coefficients[n]:.4f}{variables[n+1]} + ", end="")
+    print(f"{intercept:.4f}")
+    
+    # Calculate and output r-squared
+    SSR = 0
+    for i in residuals:
+        SSR += i ** 2
+    y_sum = 0
+    for i in data_dict[variables[0]]:
+        y_sum += i
+    y_mean = y_sum / len(data_dict[variables[0]])
+    SST = 0
+    for i in data_dict[variables[0]]:
+        SST += (i - y_mean) ** 2
+    print(f"R squared: {1 - (SSR / SST):.4f}")
+
+def main():
+    # handle file input
+    filename = str(input("Enter the name of your file: "))
+    try:
+        data_dict, variables = input_data(filename)
+    except:
+        print("There was a problem with opening or parsing the file.")
+        return
+    
+    # run regression
+    coefficients, intercept, residuals = multiple_regression(data_dict, variables)
+    
+    # output results
+    output(data_dict, variables, coefficients, intercept, residuals)
+    
+if __name__ == "__main__":
+    main()
